d4d21ff0aabb63caeb3cb020ed0e31d91c52cf66
Alejandro Amar <alejandroamar66@gmail.com>
2021-01-28 19:29:37 -0500

    Ball chase Udacity first change

---
+++ catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(ball_chaser_MESSAGE_FILES "")
+set(ball_chaser_SERVICE_FILES "/media/aleamar264/Varios/Maestria/Maestria/Curso/Software_Engineer_Robotics/project_2/catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
